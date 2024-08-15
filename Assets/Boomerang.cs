9c5257cceaa646e8f4007e855577e346e44f8679
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-08-15 12:46:33 -0400

    Commit #3

---
+++ Assets/Boomerang.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boomerang : MonoBehaviour
+{
+    public float speed = 10f;
+
+    public float maxDistance = 10f;
+    public bool homing = true;
+    public float homingForce = 2f;
+
+    private Rigidbody2D rb;
+    private Vector2 initialPosition;
+    private bool returning = false;
+    private Transform player;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        initialPosition = transform.position;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb.velocity = transform.up * speed;
+    }
+
+    void Update()
+    {
+        if (!returning && Vector2.Distance(initialPosition, transform.position) >= maxDistance)
+        {
+            returning = true;
+
+        }
+
+        if (returning && homing)
+        {
+            Vector2 directionToPlayer = (player.position - transform.position).normalized;
+            rb.velocity = Vector3.Lerp(rb.velocity,(directionToPlayer * homingForce * Vector2.Distance(player.position, transform.position)),Time.deltaTime);
+        }
+
+        if (rb.velocity != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        }
+        if (Vector2.Distance(transform.position, player.position) < 1.2f) { Destroy(gameObject); }
+    }
+
+
+}
